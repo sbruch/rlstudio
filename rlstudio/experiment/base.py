98976257c7e521df9b1e23005ecf59a6d8020d31
Bruch <s@bruch.io>
2020-12-22 09:41:37 -0500

    Add an experiment config

--- rlstudio/experiment/base.py
+++ rlstudio/experiment/base.py
@@ -8,3 +8,19 @@ class EvaluationMetadata:
   env_id: TaskId
   round_id: RoundId
   episode_id: EpisodeId
+
+
+@dataclass
+class Configuration:
+  """Training and test configuration.
+
+  repeat: Number of times to repeat the series of tasks.
+  episodes: Number of (training and test) episodes per task.
+  train_eval_step: Evaluation step size during training.
+      Agent is evaluated every `eval_step` episodes of training.
+  reset_agent: Whether to call `reset()` on the agent at the beginning of every episode.
+  """
+  repeat: int = 0
+  episodes: int = 100
+  train_eval_step: int = 1
+  reset_agent: bool = True
--- rlstudio/experiment/framework.py
+++ rlstudio/experiment/framework.py
@@ -8,22 +8,19 @@ from typing import List
 
 class Experiment:
   """Defines an experiment."""
-  def __init__(self, agent: agent_base.Agent):
+  def __init__(self,
+               config: exp_base.Configuration,
+               agent: agent_base.Agent):
+    self.config = config
     self.agent = agent
 
   def train(self,
             tasks: List[env_base.Task],
-            repeat: int = 0,
-            episodes: int = 100,
-            eval_step: int = 1,
             summary: record.Summary = None) -> None:
     """Trains the agent on a series of tasks.
 
     Args:
       tasks: A list of tasks to train the agent on.
-      repeat: Number of times to repeat the series of tasks.
-      episodes: Number of training episodes per task.
-      eval_step: Evaluation step size. The agent is evaluated every `eval_step` episodes.
       summary: An optional `Summary` module to record statistics.
     """
     if len(tasks) == 0:
@@ -33,13 +30,13 @@ class Experiment:
         raise ValueError('All tasks used for training must be of the same type')
 
     time = -1
-    for round_id in range(repeat + 1):
+    for round_id in range(self.config.repeat + 1):
       for task in tasks:
-        print(f'Training agent for {episodes} episodes on task {task.id()}')
+        print(f'Training agent for {self.config.episodes} episodes on task {task.id()}')
     
-        for episode in range(episodes):
+        for episode in range(self.config.episodes):
           # Evaluation.
-          if not episode % eval_step:
+          if not episode % self.config.train_eval_step:
             time += 1
             metadata = exp_base.EvaluationMetadata(time, task.id(), round_id, episode)
             returns = self._eval(metadata, summary, task)
@@ -47,7 +44,8 @@ class Experiment:
 
           # Training
           timestep = task.reset()
-          self.agent.reset()
+          if self.config.reset_agent:
+            self.agent.reset()
 
           while not timestep.last():
             decision = self.agent.decide(timestep, greedy=False)
@@ -57,7 +55,8 @@ class Experiment:
 
         # Post-training evaluation.
         time += 1
-        metadata = exp_base.EvaluationMetadata(time, task.id(), round_id, episodes)
+        metadata = exp_base.EvaluationMetadata(
+          time, task.id(), round_id, self.config.episodes)
         returns = self._eval(metadata, summary, task)
         print(f'Final eval: Returns: {returns:.2f}')
 
@@ -70,12 +69,16 @@ class Experiment:
     for task in tasks:
       if type(task) is not type(tasks[0]):
         raise ValueError('All tasks used for testing must be of the same type')
-    
-    for task_idx, task in tasks:
-      metadata = exp_base.EvaluationMetadata(
-        time=0, env_id=task.id(), round_id=0, episode_id=task_idx)
-      returns = self._eval(metadata, summary, task)
-      print(f'Tested agent on task {task.id()}: Total return is {returns:.2f}')
+
+    time = -1
+    for round_id in range(self.config.repeat + 1):
+      for task_idx, task in tasks:
+        for episode in range(self.config.episodes):
+          time += 1
+          metadata = exp_base.EvaluationMetadata(
+            time, task.id(), round_id, episode_id)
+          returns = self._eval(metadata, summary, task)
+          print(f'Tested agent on task {task.id()}: Total return is {returns:.2f}')
 
   def _eval(self,
             metadata: exp_base.EvaluationMetadata,
@@ -90,7 +93,8 @@ class Experiment:
 
     # Start a new episode and reset the initial state of the agent.
     timestep = task.reset()
-    self.agent.reset()
+    if self.config.reset_agent:
+      self.agent.reset()
 
     while not timestep.last():
       decision = self.agent.decide(timestep, greedy=True)
