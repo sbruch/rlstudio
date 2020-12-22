66cb279c6dc6cf74010eb549a119442918f93a68
Bruch <s@bruch.io>
2020-12-22 10:04:35 -0500

    Move more config elements into experiment.base.Configuration

--- rlstudio/experiment/base.py
+++ rlstudio/experiment/base.py
@@ -1,6 +1,9 @@
+from rlstudio.environment import base as env_base
 from rlstudio.typing import EpisodeId, RoundId, TaskId, Time
 
 from dataclasses import dataclass
+from typing import List
+
 
 @dataclass
 class EvaluationMetadata:
@@ -14,13 +17,37 @@ class EvaluationMetadata:
 class Configuration:
   """Training and test configuration.
 
-  repeat: Number of times to repeat the series of tasks.
-  episodes: Number of (training and test) episodes per task.
+  train_tasks: Series of tasks for training.
+  train_episodes: Number of training episodes per task.
   train_eval_step: Evaluation step size during training.
       Agent is evaluated every `eval_step` episodes of training.
+
+  test_tasks: Series of tasks for testing.
+  test_episodes: Number of test episodes per task.
+
+  repeat: Number of times to repeat the series of tasks.
   reset_agent: Whether to call `reset()` on the agent at the beginning of every episode.
   """
-  repeat: int = 0
-  episodes: int = 100
+  train_tasks: List[env_base.Task],
+  train_episodes: int = 100
   train_eval_step: int = 1
+
+  test_tasks: List[env_base.Task],
+  test_episodes: int = 1
+
+  repeat: int = 0
   reset_agent: bool = True
+
+  def validate(self):
+    """Validates the configuration, raising an exception if invalid."""
+    if len(train_tasks) == 0:
+      raise ValueError('At least one task must be provided to train the agent on')
+    for task in train_tasks:
+      if type(task) is not type(train_tasks[0]):
+        raise ValueError('All tasks used for training must be of the same type')
+
+    if len(test_tasks) == 0:
+      raise ValueError('At least one task must be provided to test the agent on')
+    for task in test_tasks:
+      if type(task) is not type(test_tasks[0]):
+        raise ValueError('All tasks used for testing must be of the same type')
--- rlstudio/experiment/framework.py
+++ rlstudio/experiment/framework.py
@@ -3,8 +3,6 @@ from rlstudio.environment import base as env_base
 from rlstudio.experiment import base as exp_base
 from rlstudio.experiment import record
 
-from typing import List
-
 
 class Experiment:
   """Defines an experiment."""
@@ -13,28 +11,16 @@ class Experiment:
                agent: agent_base.Agent):
     self.config = config
     self.agent = agent
+    self.config.validate()
 
-  def train(self,
-            tasks: List[env_base.Task],
-            summary: record.Summary = None) -> None:
-    """Trains the agent on a series of tasks.
-
-    Args:
-      tasks: A list of tasks to train the agent on.
-      summary: An optional `Summary` module to record statistics.
-    """
-    if len(tasks) == 0:
-      raise ValueError('At least one task must be provided to train the agent on')
-    for task in tasks:
-      if type(task) is not type(tasks[0]):
-        raise ValueError('All tasks used for training must be of the same type')
-
+  def train(self, summary: record.Summary = None) -> None:
+    """Trains the agent on training tasks and optionally records statistics in `summary`."""
     time = -1
     for round_id in range(self.config.repeat + 1):
-      for task in tasks:
-        print(f'Training agent for {self.config.episodes} episodes on task {task.id()}')
+      for task in self.config.train_tasks:
+        print(f'Training agent for {self.config.train_episodes} episodes on task {task.id()}')
     
-        for episode in range(self.config.episodes):
+        for episode in range(self.config.train_episodes):
           # Evaluation.
           if not episode % self.config.train_eval_step:
             time += 1
@@ -56,24 +42,16 @@ class Experiment:
         # Post-training evaluation.
         time += 1
         metadata = exp_base.EvaluationMetadata(
-          time, task.id(), round_id, self.config.episodes)
+          time, task.id(), round_id, self.config.train_episodes)
         returns = self._eval(metadata, summary, task)
         print(f'Final eval: Returns: {returns:.2f}')
 
-  def test(self,
-           tasks: List[env_base.Task],
-           summary: record.Summary = None) -> None:
-    """Tests an agent on a set of tasks and optionally records statistics in `summary`."""
-    if len(tasks) == 0:
-      raise ValueError('At least one task must be provided to test the agent on')
-    for task in tasks:
-      if type(task) is not type(tasks[0]):
-        raise ValueError('All tasks used for testing must be of the same type')
-
+  def test(self, summary: record.Summary = None) -> None:
+    """Tests an agent on test tasks and optionally records statistics in `summary`."""
     time = -1
     for round_id in range(self.config.repeat + 1):
-      for task_idx, task in tasks:
-        for episode in range(self.config.episodes):
+      for task_idx, task in self.config.test_tasks:
+        for episode in range(self.config.test_episodes):
           time += 1
           metadata = exp_base.EvaluationMetadata(
             time, task.id(), round_id, episode_id)
